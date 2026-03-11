640667950d1a78fc631e8911b4fa96ecba508ec9
Elmer Cruz <elmerjesus.cruz@unir.net>
2026-03-11 14:22:21 -0500

    [FIX] descripción para la impresion de facturas

--- pacifico_base/models/account_move.py
+++ pacifico_base/models/account_move.py
@@ -7,6 +7,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _get_description_without_product_code(self, product, description):
+        description = ""
         if product and product.default_code:
             description = str(description).replace("[" + product.default_code + "]", "").strip()
         return f"{description} {product.receipt or ''}-{product.item or ''}"
