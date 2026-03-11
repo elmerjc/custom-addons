8842015baae073d3665af590fd53990284c315c8
Elmer Cruz <elmerjesus.cruz@unir.net>
2026-03-11 14:26:01 -0500

    [FIX] descripción de las facturas

--- pacifico_base/models/account_move.py
+++ pacifico_base/models/account_move.py
@@ -7,7 +7,8 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _get_description_without_product_code(self, product, description):
-        description = ""
+        name = ""
         if product and product.default_code:
             description = str(description).replace("[" + product.default_code + "]", "").strip()
-        return f"{description} {product.receipt or ''}-{product.item or ''}"
+        name = f"{description} {product.receipt or ''}-{product.item or ''}"
+        return name.strip()
